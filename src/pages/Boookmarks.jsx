12e3a22da1d298a7408fdd086e0268ace81652af
ravindra <123213831+cypher1ravi@users.noreply.github.com>
2024-05-08 23:49:22 +0530

    fix

--- src/pages/Boookmarks.jsx
+++ src/pages/Boookmarks.jsx
@@ -11,7 +11,10 @@ const Boookmarks = () => {
 
   return (
     <section className="px-4 pb-12 lg:pl-32">
+      <SearchBar
 
+        placeholder="Search for movies"
+      />
       <div className="pt-10 mb-6 font-light text-xl tracking-[-0.3125px] flex gap-x-2 items-center ">
         <h1 className="text-xl">Bookmarks</h1>
         <p className="border font-medium py-[1px] text-xs px-2 rounded-md">
@@ -21,7 +24,7 @@ const Boookmarks = () => {
       <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
         {/* Map through movies and show Skeleton Loader when loading  */}
         {loadingBookmark
-          ? [...Array(20)].map((_, i) => <SkeletonLoaderMovies key={i} />)
+          ? [...Array(3)].map((_, i) => <SkeletonLoaderMovies key={i} />)
           : bookmark.map((movie) => (
             <Link key={movie.id} to={`/bookmark/${movie.id}`}>
 
